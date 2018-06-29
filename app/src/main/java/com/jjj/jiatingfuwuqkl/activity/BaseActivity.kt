59e54d47686683578bce666c39e0857f8f5dc912
isswm <645187582@qq.com>
2018-06-29 10:34:36 +0800

    上传目录内容

--- app/src/main/java/com/jjj/jiatingfuwuqkl/activity/BaseActivity.kt
+++ app/src/main/java/com/jjj/jiatingfuwuqkl/activity/BaseActivity.kt
@@ -5,22 +5,27 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.global_header.*
 
 open class BaseActivity : AppCompatActivity() {
-    // 设置头部箭头返回事件
-    fun setBackFinish(view: View) {
-        view.visibility = View.VISIBLE
-        view.setOnClickListener({
+    // 设置头部 箭头返回事件
+    fun setBackBtnFinish() {
+        header_back.visibility = View.VISIBLE
+        header_back.setOnClickListener({
             finish()
         })
     }
-
-    // 设置头部按钮的点击事件
-    fun setBtnClick(view: View, clickListener: View.OnClickListener) {
-        view.visibility = View.VISIBLE
-        view.setOnClickListener(clickListener)
+    // 设置头部 标题内容
+    fun setTitle(text:String){
+        header_title.text = text
     }
 
+    // 设置头部 按钮点击事件
+    fun setBtnClick(clickListener: View.OnClickListener) {
+        header_btn.visibility = View.VISIBLE
+        header_btn.setOnClickListener(clickListener)
+    }
+    // 启动activity 使用 mStartActivity(context,*::class.java)
     fun mStartActivity(packageContext: Context, cls:Class<*> ){
         packageContext.startActivity(Intent(packageContext, cls))
     }
