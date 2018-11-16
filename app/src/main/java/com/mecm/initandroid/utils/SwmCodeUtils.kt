4c6a2aacacdeb1d60a0876d04d1b13a09e453e5b
mecm <645187582@qq.com>
2018-11-16 18:51:33 +0800

    验证码

--- app/src/main/java/com/mecm/initandroid/utils/SwmCodeUtils.kt
+++ app/src/main/java/com/mecm/initandroid/utils/SwmCodeUtils.kt
@@ -1,19 +1,15 @@
-//package com.mecm.initandroid.utils
-//
-//import android.widget.TextView
-//import com.mecm.art.entity.AddClassEt
-//import com.mecm.art.entity.BaseData
-//import com.mecm.initandroid.entity.BaseData
-//import io.reactivex.disposables.Disposable
-//
-//object SwmCodeUtils {
-//    /**
-//     * 判断字符串是否有值，如果为null或者是空字符串或者只有空格或者为"null"字符串，则返回true，否则则返回false
-//     */
-//    fun sendCode(timerVIew: TextView, registerPhone: TextView,type:String) {
-//        SwmCountDownTimerUtils(timerVIew, 60000, 1000).start()
-//        val regPhone = SwmTextUtils.getText(registerPhone)
-//        if (SwmRegUtils.isMobileNOToast(regPhone)) {
+package com.mecm.initandroid.utils
+
+import android.widget.TextView
+
+object SwmCodeUtils {
+    /**
+     * 判断字符串是否有值，如果为null或者是空字符串或者只有空格或者为"null"字符串，则返回true，否则则返回false
+     */
+    fun sendCode(timerVIew: TextView, registerPhone: TextView, type: String) {
+        val regPhone = SwmTextUtils.getText(registerPhone)
+        if (SwmRegUtils.isMobileNOToast(regPhone)) {
+            SwmCountDownTimerUtils(timerVIew, 60000, 1000).start()
 //            SwmRxHttpUtils(object : SwmRxHttpUtils.SwmRequestComListener {
 //                override fun onNextError() {
 //                }
@@ -22,14 +18,12 @@
 //                }
 //
 //                override fun onNext(baseData: BaseData) {
-//                    val addClassEt = baseData as AddClassEt
-////                    timerVIew.text = addClassEt.result
-////                    SwmToastUtils.showSingleLongToast(addClassEt.result)
+//
 //                }
 //
 //                override fun onComplete() {
 //                }
 //            }).getRegCode(regPhone,type)
-//        }
-//    }
-//}
+        }
+    }
+}
